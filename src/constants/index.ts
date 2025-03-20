3dee1dc2f44f7ca5d460bb9a99159111e9cca02b
abarena <angelbarena89@gmail.com>
2025-03-20 01:39:57 -0600

    Add navigation and social link constants

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+import { NavListItems } from "../models/navigation";
+import { SocialListItems } from "../models/social";
+
+export const navListItems : NavListItems = [
+  {
+    name: 'about',
+    href: '/about'
+  },
+  {
+      name: 'projects',
+      href: '/projects'
+  },
+  {
+      name: 'contact',
+      href: '/contact'
+  }
+]
+
+export const socialListItems: SocialListItems = [
+  {
+      id:'linkedin',
+      iconName: 'linkedin',
+      href: 'https://www.linkedin.com/in/angel-barena/'
+  },
+  {
+      id:'github',
+      iconName: 'github',
+      href: 'https://github.com/Angel-Barena'
+  },
+  {
+      id:'codepen',
+      iconName: 'codepen',
+      href: 'https://codepen.io/angel_barena'
+  }
+]
