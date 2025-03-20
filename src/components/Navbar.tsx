b93b230d122b2200ca5938f3fae2c2486ae10051
abarena <angelbarena89@gmail.com>
2025-03-20 01:37:13 -0600

    Add Navbar component with navigation links and logo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { Link, NavLink } from 'react-router';
+import { NavItem } from '../models/navigation';
+import { navListItems } from '../constants';
+
+type ListItemProps = {
+    item: NavItem;
+}
+
+const ListItem = ({ item }: ListItemProps) => {
+    return (
+        <li key={item.name} className="nav-list__item">
+            <NavLink
+                className={({ isActive }) => isActive
+                    ? 'active'
+                    : 'nav-list__item__link'
+                }
+                to={`${item.href}`}
+            >
+            {item.name}
+            </NavLink>
+        </li>
+    );
+}
+
+export default function NavBar() {
+    return(
+        <nav className="nav">
+            <div className="nav-logo">
+                <Link to={`/`} className="nav-logo__link">
+                    <svg className="nav-logo__icon">
+                        <defs>
+                            <mask id="mask" x="0" y="0" width="100%" height="100%">
+                            <rect id="overlay" x="0" y="0" width="100%" height="100%" />
+                            <text id="text" x="50%" y="-2" dy="35">ab</text>
+                            </mask>
+                        </defs>
+                        <rect id="r" x="0" y="0" width="100%" height="100%" />
+                    </svg>
+                </Link>
+            </div>
+            <ul className="nav-list">
+                {navListItems.map(
+                    item => <ListItem key={item.name} item={item}/>
+                )}
+            </ul>           
+        </nav>
+    );
+}
