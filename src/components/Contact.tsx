e36746e68a8ce45566b91a8c0fecf144550c49e1
abarena <angelbarena89@gmail.com>
2025-04-16 19:40:29 -0600

    Update contact information in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,10 +19,10 @@ export default function Contact() {
                 <h1 className="contact-title">Contact</h1>
                 <section className="contact-info">
                     <div className="contact-info--mail">
-                        <FontAwesomeIcon icon={"envelope-square"} /> angelbarena89@hotmail.com
+                        <FontAwesomeIcon icon={"envelope-square"} /> angelbarena89@gmail.com
                     </div>
                     <div className="contact-info--phone">
-                        <FontAwesomeIcon icon={"phone-square"} /> +52 1 833 151 3138
+                        <FontAwesomeIcon icon={"phone-square"} /> +528126002748
                     </div>
                 </section>
             </main>
